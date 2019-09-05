486ea139498e48205795d4bfc3c825998730d03c
Kim Joon <jk1094@naver.com>
2019-09-05 20:43:05 +0900

    Views

--- Views/app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ Views/app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.android.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -10,7 +14,10 @@ import android.widget.SimpleAdapter;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ListView mainList;
+    ListView viewLV;
+    ListView viewGroupLV;
+    ArrayList<String> viewList;
+    ArrayList<String> viewGroupList;
 
 
     @Override
@@ -18,13 +25,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> viewList = new ArrayList<>();
+        viewList = new ArrayList<>();
         viewList.add("TextView");
         viewList.add("ImageView");
         viewList.add("Progressbar");
 
-        mainList = (ListView) findViewById(R.id.mainLV);
+        viewGroupList = new ArrayList<>();
+        viewGroupList.add("LinearLayout");
+        viewGroupList.add("RelativeLayout");
+        viewGroupList.add("FrameLayout");
+        viewGroupList.add("AbsoluteLayout");
+        viewGroupList.add("AdapterView");
+
+        viewLV = (ListView) findViewById(R.id.mainLV);
+        viewGroupLV = (ListView) findViewById(R.id.viewGroupLV);
+
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,viewList);
-        mainList.setAdapter(arrayAdapter);
+        ArrayAdapter viewGroupAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,viewGroupList);
+
+        viewLV.setAdapter(arrayAdapter);
+        viewGroupLV.setAdapter(viewGroupAdapter);
+
+        viewLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if(i==0){
+                    Intent intent = new Intent(getApplicationContext(), TextView.class);
+                    startActivity(intent);
+                }else if(i==1){
+                    Log.d("KJ","1111");
+                }else if(i==2){
+                    Log.d("KJ","2222");
+                }
+
+            }
+        });
     }
 }
