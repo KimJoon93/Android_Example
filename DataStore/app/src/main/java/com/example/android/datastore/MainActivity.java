a7881e5c0618f6fafbeb53431678afaf0a0a5cf0
Kim Joon <jk1094@naver.com>
2019-09-10 19:35:05 +0900

    Preference

--- DataStore/app/src/main/java/com/example/android/datastore/MainActivity.java
+++ DataStore/app/src/main/java/com/example/android/datastore/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.android.datastore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String emailText;
+
+    EditText mEmailEditText;
+    CheckBox mSaveCheckbox;
+
+    private SharedPreferences mPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEmailEditText = findViewById(R.id.edit_email);
+        mSaveCheckbox = findViewById(R.id.save_checkbox);
+
+        mPreference = PreferenceManager.getDefaultSharedPreferences(this);
+
+        Boolean isEmailSaveChecked = mPreference.getBoolean("Save",false);
+        mSaveCheckbox.setChecked(isEmailSaveChecked);
+
+        if(isEmailSaveChecked){
+            String Email = mPreference.getString("Email","");
+            mEmailEditText.setText(Email);
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences.Editor editor = mPreference.edit();
+
+        editor.putBoolean("save",mSaveCheckbox.isChecked());
+        editor.apply();
+
     }
 }
