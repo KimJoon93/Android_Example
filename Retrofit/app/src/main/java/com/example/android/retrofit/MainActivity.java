fccb6f343971af927143d68ad8a5778d78a2e450
Kim Joon <jk1094@naver.com>
2019-09-16 23:48:51 +0900

    Button created

--- Retrofit/app/src/main/java/com/example/android/retrofit/MainActivity.java
+++ Retrofit/app/src/main/java/com/example/android/retrofit/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.retrofit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendBtn = findViewById(R.id.btn1);
+
+        Button receiveBtn = findViewById(R.id.btn2);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        receiveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
